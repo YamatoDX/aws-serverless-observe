9bf1d2d75094e1ab9ebb1aedb1bd8a647cc5c5bd
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-12-26 04:36:54 +0600

    console logs removed

--- lambdas/getUsers.js
+++ lambdas/getUsers.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 
 exports.handler = async (event) => {
-  console.log("event is", event);
   try {
     if (event.httpMethod !== "GET") {
       throw new Error("Must be a GET Request");
