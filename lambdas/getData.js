ee08758e5014a790ca962bf467339ef9ec945330
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-12-26 12:07:14 +0600

    single function created

--- lambdas/getData.js
+++ lambdas/getData.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-exports.getUsers = async (event) => {
+exports.handler = async (event) => {
   try {
     if (event.httpMethod !== "GET") {
       throw new Error("Must be a GET Request");
@@ -20,45 +20,6 @@ exports.getUsers = async (event) => {
     );
     return getReturnValue(200, data);
   } catch (err) {
-    const finalData = {
-      message: err.message,
-      data: [],
-    };
-    return getReturnValue(400, finalData);
-  }
-};
-
-exports.getPosts = async (event) => {
-  console.log("event is", event);
-  try {
-    if (event.httpMethod !== "GET") {
-      throw new Error("Must be a GET Request");
-    }
-    console.log("one");
-    const { queryStringParameters } = event;
-    const { id } = queryStringParameters;
-    console.log("twp");
-    if (!id) {
-      throw new Error("id is missing");
-    }
-    console.log("three");
-    if (Number(id) < 0 || Number(id) > 10) {
-      throw new Error("invalid id has been entered");
-    }
-    console.log("four");
-    const { data: responseData } = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts`
-    );
-    console.log("five");
-    const data =
-      responseData?.filter((eachPost) => eachPost["userId"] === id) ?? [];
-    console.log("six");
-    console.log("data is", data);
-    console.log("getReturnValue is", getReturnValue(200, data));
-    return getReturnValue(200, data);
-  } catch (err) {
-    console.log("seven");
-    console.error(err);
     const finalData = {
       message: err.message,
       data: [],
