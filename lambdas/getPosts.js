7aefcd2b77bfd0bd4bb9daa34314743fa4452798
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-12-26 11:31:22 +0600

    getPosts added

---
+++ lambdas/getPosts.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+
+exports.handler = async (event) => {
+  try {
+    if (event.httpMethod !== "GET") {
+      throw new Error("Must be a GET Request");
+    }
+    const { queryStringParameters } = event;
+    const { userId: id } = queryStringParameters;
+
+    if (!id) {
+      throw new Error("id is missing");
+    }
+
+    if (Number(id) < 0 || Number(id) > 10) {
+      throw new Error("invalid id has been entered");
+    }
+    const { data: responseData } = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts`
+    );
+    const data = responseData?.filter((eachPost) => eachPost.userId === id);
+    return getReturnValue(200, data);
+  } catch (err) {
+    const finalData = {
+      message: err.message,
+      data: [],
+    };
+    return getReturnValue(400, finalData);
+  }
+};
+
+function getReturnValue(statusCode = 400, finalData = {}) {
+  return {
+    isBase64Encoded: false,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Origin": "*",
+    },
+    statusCode,
+    body: JSON.stringify(finalData),
+  };
+}
